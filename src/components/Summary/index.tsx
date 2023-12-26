4e8eb7d6af9c956140d6f778a6e91cbd11cb738b
reciokaue <kaue.recio2@gmail.com>
2023-12-26 11:06:04 -0300

    useMemo for performance example

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,28 +2,31 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { SummaryCard, SummaryContainer } from './styles'
 import { useTransactions } from '../../contexts/Transactions'
 import { priceFormatter } from '../../utils/formatter'
+import { useMemo } from 'react'
 
 export function Summary() {
   const { transactions } = useTransactions()
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return (
     <SummaryContainer>
