63c580056891e605c91e9a2e2b220b8df325dbab
reciokaue <kaue.recio2@gmail.com>
2023-12-26 07:58:17 -0300

    search form schema

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,12 +1,42 @@
-import { MagnifyingGlass } from 'phosphor-react'
-import { SearchFormContainer } from './styles'
+import { CircleNotch, MagnifyingGlass } from 'phosphor-react'
+import { SearchFormContainer, SpinningIcon } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
-      <input placeholder="Busque por transações" />
-      <button type="submit">
-        <MagnifyingGlass size={20} />
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+      <button type="submit" disabled={isSubmitting}>
+        {isSubmitting ? (
+          <SpinningIcon size={20} />
+        ) : (
+          <MagnifyingGlass size={20} />
+        )}
         Buscar
       </button>
     </SearchFormContainer>
--- src/components/SearchForm/styles.ts
+++ src/components/SearchForm/styles.ts
@@ -1,3 +1,4 @@
+import { CircleNotch } from 'phosphor-react'
 import styled from 'styled-components'
 
 export const SearchFormContainer = styled.form`
@@ -29,11 +30,42 @@ export const SearchFormContainer = styled.form`
     border-radius: 6px;
     cursor: pointer;
 
-    &:hover {
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       border-color: ${(props) => props.theme['green-500']};
       background-color: ${(props) => props.theme['green-500']};
       color: ${(props) => props.theme.white};
       transition: 200ms;
+    }
+  }
+`
+export const SpinningIcon = styled(CircleNotch)`
+  -webkit-animation: icon-spin 2s infinite linear;
+  animation: icon-spin 2s infinite linear;
+
+  @-webkit-keyframes icon-spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(359deg);
+      transform: rotate(359deg);
+    }
+  }
+
+  @keyframes icon-spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(359deg);
+      transform: rotate(359deg);
     }
   }
 `
